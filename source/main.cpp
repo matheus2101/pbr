ac14d8d5e2a07251280e502b7fe43f0ca6988c59
Matheus <matheus-2101@hotmail.com>
2018-03-11 22:41:20 -0300

    pinhole camera

--- source/main.cpp
+++ source/main.cpp
@@ -5,12 +5,12 @@ int main( void )
     unsigned int x_resolution = 512;
     unsigned int y_resolution = 512;
 
-    OrthographicCamera camera{ -1.25f, 
+    PinholeCamera camera{ -1.25f, 
                                 1.25f, 
                                -1.25f, 
                                 1.25f,
                                 glm::ivec2{ x_resolution, y_resolution }, 
-                                glm::vec3{ 0.0f, 0.0f,  1.0f },     // position
+                                glm::vec3{ 0.0f, 0.0f,  2.0f },     // position
                                 glm::vec3{ 0.0f, 1.0f,  0.0f },     // up
                                 glm::vec3{ 0.0f, 0.0f, -1.0f } };   // look at
     Scene scene{};
---
+++ source/pinhole_camera.cpp
@@ -0,0 +1,36 @@
+#include "pinhole_camera.h"
+
+PinholeCamera::PinholeCamera( void )
+{}
+
+PinholeCamera::PinholeCamera( const float min_x,
+                                        const float max_x,
+                                        const float min_y,
+                                        const float max_y,
+                                        const glm::ivec2 &resolution,
+                                        const glm::vec3 &position,
+                                        const glm::vec3 &up_vector,
+                                        const glm::vec3 &look_at ) :
+        Camera::Camera{ resolution, 
+                        position,
+                        up_vector,
+                        look_at },
+        min_x_{ min_x },
+        max_x_{ max_x },
+        min_y_{ min_y },
+        max_y_{ max_y }
+{}
+
+Ray PinholeCamera::getWorldSpaceRay( const glm::vec2 &pixel_coord ) const
+{
+    float width = max_x_ - min_x_;
+    float height = max_y_ - min_y_;
+
+    glm::vec3 s{ min_x_ + (width * ((pixel_coord[0] + 0.5f) / resolution_[0])),
+                 min_y_ + (height * ((pixel_coord[1] + 0.5f) / resolution_[1])),
+                 -1.0f};
+
+    return Ray{ position_, 
+                glm::normalize( s - position_) };
+}
+
